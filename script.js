15213cd9704b01ee99066e220d01bea80b945b71
Harsh-Rohilla <124336289+Harsh-Rohilla@users.noreply.github.com>
2023-12-06 12:44:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function redirectToDestinations() {
+    window.location.href = "destinations.html";
+  }
+  
+  function submitForm(event) {
+    event.preventDefault();
+    alert("Form submitted! (This is a dummy alert. In a real project, you would handle form submission.)");
+  }
+  
